b4afd69554422f59b7635980f21512d684c00771
ketjj <jorjolianiket@gmail.com>
2022-05-18 02:44:05 +0200

    adding load

---
+++ src/components/LoadComps.vue
@@ -0,0 +1,115 @@
+<template>
+<div>
+  <!-- {{loadStatus}} -->
+  <div class="loader2"></div>
+  <div class="loader"></div>
+</div>
+  
+</template>
+
+<script>
+export default {
+  name: 'LoadComps',
+  props: {
+    // loadStatus:String
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+div{
+  font-size: 20px;
+  color: white;
+  .loader {
+      width: 100px;
+      height: 100px;
+      margin: 0 auto;
+      margin-top: 20px;
+      position: relative;
+      background: #FFF;
+      border-radius: 4px;
+      overflow: hidden;
+    }
+    .loader:before {
+      content: "";
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      width: 100px;
+      height: 100px;
+      transform: rotate(45deg) translate(30%, 40%);
+      background: #ff9371;
+      box-shadow: 32px -34px 0 5px #ff3d00;
+      animation: slide 2s infinite ease-in-out alternate;
+    }
+    .loader:after {
+      content: "";
+      position: absolute;
+      left: 10px;
+      top: 10px;
+      width: 16px;
+      height: 16px;
+      border-radius: 50%;
+      background: #ff3d00;
+      transform: rotate(0deg);
+      transform-origin: 35px 145px;
+      animation: rotate 2s infinite ease-in-out;
+    }
+
+    @keyframes slide {
+      0% , 100%{ bottom: -35px}
+      25% , 75%{ bottom: -2px}
+      20% , 80%{ bottom: 2px}
+    }
+    @keyframes rotate {
+      0% { transform: rotate(-15deg)}
+      25% , 75%{ transform: rotate(0deg)}
+      100% {  transform: rotate(25deg)}
+    }
+
+    .loader2{
+      font-size: 48px;
+      color: #FFF;
+      display: inline-block;
+      font-family: Arial, Helvetica, sans-serif;
+      font-weight: 400;
+      position: relative;
+    }
+    .loader2:before{
+      content: '';
+      animation: 5s print linear alternate infinite;
+    }
+    .loader2:after{
+      content: '';
+      position: absolute;
+      right: -4px;
+      top: 50%;
+      transform: translatey(-45%);
+      width: 2px;
+      height: 1.3em;
+      background: currentColor;
+      opacity: 0.8;
+      animation: 1s blink steps(2) infinite;
+    }
+    
+    @keyframes blink {
+      0%  { visibility: hidden;}
+      100%  { visibility: visible;}
+    }
+    @keyframes print {
+      0% { content: 'L'}
+      10% { content: 'Lo'}
+      20% { content: 'Loa'}
+      30% { content: 'Load'}
+      40% { content: 'Loadi'}
+      50% { content: 'Loadin'}
+      60% { content: 'Loading'}
+      70% { content: 'Loading.'}
+      80% { content: 'Loading..'}
+      90% , 100% { content: 'Loading...'}
+    }
+  
+}
+
+</style>
--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -1,14 +1,22 @@
 <template>
 <div class="k_container py-5">
-  <AlbumComps 
-  v-for="(album,index) in albums" :key="`album${index}`"
-  :albumItem="album"/>
+  <div v-if="isLoad" class="album-container">
+    <AlbumComps 
+    v-for="(album,index) in albums" :key="`album${index}`"
+    :albumItem="album"
+    />
+  </div>
+  <div v-else class="album-container2">
+    <LoadComps /> 
+    <!-- loadStatus="Loading... please wait!" -->
+  </div>
 
 </div>
   
 </template>
 
 <script>
+import LoadComps from '@/components/LoadComps' 
 import AlbumComps from '@/components/AlbumComps' 
 // con ./ mi dava l'errore
 import axios from 'axios';
@@ -17,11 +25,13 @@ export default {
   name: 'MainComp',
   components:{
     AlbumComps,
+    LoadComps
   },
   data(){
     return{
       apiUrl: 'https://flynn.boolean.careers/exercises/api/array/music',
-      albums: []
+      albums: [],
+      isLoad: false
     }
   },
   mounted(){
@@ -33,6 +43,7 @@ export default {
       axios.get(this.apiUrl)
       .then(res =>{
         this.albums = res.data.response;
+        this.isLoad = true;
       })
     }
   }
@@ -44,9 +55,12 @@ export default {
 <style lang="scss" scoped>
 @import '../assets/style/general';
 @import '../assets/style/vars';
-.k_container{
+.album-container{
   display: flex;
   flex-wrap: wrap;
+}
+.album-container2{
+  text-align: center;
 }
 
 </style>
